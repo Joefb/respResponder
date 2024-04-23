f0f1836464cb2e354ec400a771487f4ac72ac22e
Joe Burgess <joeezmail@gmail.com>
2024-04-23 00:25:29 -0700

    initial init for test branch

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
+<div align="center">
 # respResponder
+###### Voice notifications so you never miss a hash!
+</div>
+
+## - TOC
+* [The Issue](#-The-Issue)
+* [The Fix](#-The-Fix)
+* [Installation](#-Installation)
+* [Usage](#-Usage)
+
+## - The Issue
+You fire up Responder for some LLMNR poisoning so you can capture some sweet sweet hashes. You leave Responder running in
+the background and continue haxoring the network but totally forget that it's still running. Two hours later... You finally realize Responder is
+still running and check it. O snap! Multiple hashes were captured an hour and a half ago! You really wish you were notified right away.
+
+## - The Fix
+Use a handy python script that will give voice notifications when a hash is captured!
+
+## - Installation
+* Python3 required
+* Playsound python package required
+```bash
+pip install playsound
+```
+* If Responder is not installed to default location then open 'respResponder.py` in your favorite editor and add the path on line 8:
+```python
+### If Responder was not installed to the default location enter log path below ###
+##Custom log path:
+log_path = "./Responder-Session.log"
+```
+## - Usage
+* Run `respResponder.py`
+```python
+python3 respResponder.py
+```
+* Run Responder
+```bash
+sudo responder -I eth0 -dPv
+```
+
---
+++ respResponder.py
@@ -0,0 +1,75 @@
+import os
+import time
+import datetime
+from playsound import playsound
+
+### If Responder was not installed to the default location enter log path below ###
+##Custom log path:
+#log_path = "./Responder-Session.log"
+
+
+### No need to modify below this line ########################################
+##############################################################################
+
+##Default installation log path:
+log_path = "/usr/share/responder/logs/Responder-Session.log"
+
+file_size = os.path.getsize(log_path)
+last_pos = file_size
+last_password = ""
+last_user_name = ""
+time_captured = ""
+
+
+def read_log(log_path, last_pos):
+    with open(log_path, "r") as f:
+        f.seek(last_pos)
+        new_lines = f.readlines()
+        last_pos = f.tell()
+    return new_lines, last_pos
+
+
+#### Main loop ###
+try:
+    while True:
+        time.sleep(0.5)
+        os.system("clear")
+        file_size = os.path.getsize(log_path)
+
+        if file_size != last_pos:
+            new_lines, last_pos = read_log(log_path, last_pos)
+            for line in new_lines:
+                line = line.strip()
+
+                if "NTLMv2-SSP Hash" in line:
+                    split_hash = line.split(":")
+                    captured_user_name = split_hash[3].strip()
+
+                    if captured_user_name != last_user_name:
+                        playsound("./hash_capture.mp3")
+                        split_hash = line.split()
+                        last_password = split_hash[8]
+                        last_user_name = captured_user_name
+                        time_captured = datetime.datetime.now()
+                        time_captured = time_captured.strftime("%m/%d/%Y %I:%M:%S %p") 
+                        break
+
+                last_pos = file_size
+
+        print(f'''\033[0;32m
+
+RespResponder
+===================================================================
+Log File: {log_path}
+
+Capture Time     : {time_captured}
+Captured Username: {last_user_name}
+Captured Hash    : {last_password}
+==================================================================
+
+Press Ctrl+C to exit.
+                ''')
+
+except KeyboardInterrupt:
+    print("Exiting program!")
+
